ec983dfc0ddfc1808e2de475f6bc8924357f8585
Noman Haider <noman567n@gmail.com>
2025-09-20 09:20:58 +0500

    envs change

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,4 +1,4 @@
 export const environments = {
     production: true,
-    API_BASE_URL: 'https://project-management-backend-server.vercel.app/api/v1'
+    API_BASE_URL: 'https://project-management-backend-peach.vercel.app/api/v1'
 }
